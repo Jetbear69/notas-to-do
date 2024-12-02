d09d5e19ee9445389f653c1f6b2011db5cdc7123
Oliver.dev <ohuamanm@gmail.com>
2024-12-02 10:34:38 -0500

    Update Login.jsx

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     //Logueo y llamado de la api
 
     try {
-      const response = await axiosInstance.post('https://notas-to-do-eta.vercel.app', {
+      const response = await axiosInstance.post('/login', {
         email: email,
         password: password,
       });
