a390773b870679e50f8c38d28638e63edb43120a
Oliver.dev <ohuamanm@gmail.com>
2024-12-02 11:00:20 -0500

    Update axiosInstance.js

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BASE_URL } from './constants';
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: 'https://notas-to-do-api.vercel.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
